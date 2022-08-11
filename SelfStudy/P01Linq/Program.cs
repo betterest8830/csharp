7c8baf3b932dcec5578f9c8517778a9c007443a7
Chunlong Xu <chunlongxu@microsoft.com>
2022-08-11 16:29:15 +0800

    work_2022.08.11

---
+++ SelfStudy/P01Linq/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testData = { 5, 2, 0, 66, 4, 32, 7, 1 };
+            TestLinq.TestLinq1(testData);
+            TestLinq.TestLinq2(testData);
+            TestLinq.TestLinq3(testData);
+
+        }
+    }
+
+    class TestLinq
+    {
+        // 在C#2.0以前，假设有一个整数类型的数组，找到里面的偶数并进行降序排序。
+        public static void TestLinq1(int[] testData)
+        {
+            List<int> newList = new List<int>();
+            foreach (var item in testData)
+            {
+                if(item % 2 == 0)
+                {
+                    newList.Add(item);
+                }
+            }
+            newList.Sort();
+            newList.Reverse();
+            Console.WriteLine(String.Join(",", newList));
+        }
+
+        // C#2.0引入了delegate，可以使用委托来处理这种场景
+        public delegate bool FindEven(int item);
+        public static int[] where(int[] testData, FindEven delete)
+        {
+            int[] newArray = new int[5];
+            int i = 0;
+            foreach (var item in testData)
+            {
+                if (delete(item))
+                {
+                    newArray[i] = item;
+                    i++;
+                }
+            }
+            return newArray;
+        }
+        public static void TestLinq2(int[] testData)
+        {
+            List<int> newList = where(testData, delegate (int item)
+            {
+                return item % 2 == 0;
+            }).ToList();
+            newList.Sort();
+            newList.Reverse();
+            Console.WriteLine(String.Join(",", newList));
+        }
+
+        // 在C#3.0中引入了扩展方法、Lambda表达式、匿名类型等新特性，你可以使用C#3.0的这些新特性
+        public static void TestLinq3(int[] testData)
+        {
+            List<int> evens = testData.Where(p => p % 2 == 0).ToList();
+            evens.Sort();
+            evens.Reverse();
+            int[] odds = testData.Where(P => P % 2 != 0).ToArray();
+            Console.WriteLine("evens: " + string.Join(",", evens));
+            Console.WriteLine("odds: " + string.Join(",", odds));
+        }
+
+    }
+
+}
---
+++ SelfStudy/P02Generic/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace P02Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            CommonMethod.ShowObject(1);
+            CommonMethod.ShowObject("1");
+            CommonMethod.Show<int>(1);
+            CommonMethod.Show<string>("1");
+            Monitor.Show();
+        }
+    }
+
+    public class CommonMethod
+    {
+        // 使用Object类型达到了我们的要求，解决了代码的可复用
+        public static void ShowObject(object o)
+        {
+            Console.WriteLine("This is {0}, parameter={1}, type={2}", 
+                typeof(CommonMethod), o.GetType().Name, o);
+        }
+
+        // 上面object类型的方法又会带来另外一个问题：装箱和拆箱，会损耗程序的性能
+        // 微软在C#2.0的时候推出了泛型，可以很好的解决上面的问题
+        // 泛型方法
+        public static void Show<T>(T t)
+        {
+            Console.WriteLine("This is {0}, parameter={1}, type={2}",
+                typeof(CommonMethod), t.GetType().Name, t);
+        }
+
+    }
+
+    public class Monitor
+    {
+        public static void Show()
+        {
+            Console.WriteLine("********monitor start*******");
+            int value = 1234;
+            long commonSecond=0, objectSecond=0, genericSecond=0;
+            
+            {
+                Stopwatch watch = new Stopwatch();
+                watch.Start();
+                for (int i = 0; i < 1000000000; i++)
+                {
+                    ShowInt(value);
+                }
+                watch.Stop();
+                commonSecond = watch.ElapsedMilliseconds;
+            }
+
+            {
+                Stopwatch watch = new Stopwatch();
+                watch.Start();
+                for (int i = 0; i < 1000000000; i++)
+                {
+                    ShowObject(value);
+                }
+                watch.Stop();
+                objectSecond = watch.ElapsedMilliseconds;
+            }
+
+            {
+                Stopwatch watch = new Stopwatch();
+                watch.Start();
+                for (int i = 0; i < 1000000000; i++)
+                {
+                    Show<int>(value);
+                }
+                watch.Stop();
+                genericSecond = watch.ElapsedMilliseconds;
+            }
+
+            Console.WriteLine("commonSecond={0},objectSecond={1},genericSecond={2}", commonSecond, objectSecond, genericSecond);
+            Console.WriteLine("********monitor end*******");
+        }
+
+        #region PrivateMethod
+        private static void ShowInt(int iParameter)
+        {
+            //do nothing
+        }
+        private static void ShowObject(object oParameter)
+        {
+            //do nothing
+        }
+        private static void Show<T>(T tParameter)
+        {
+            //do nothing
+        }
+        #endregion
+    }
+
+    // 泛型类
+
+    // 泛型接口
+
+    // 泛型委托
+
+    // 泛型约束
+
+    // 泛型的协变和逆变
+
+    // 泛型缓存
+
+
+}

b61e8715ad57b506936f2d1208828f55835893da
Chunlong Xu <chunlongxu@microsoft.com>
2022-08-12 17:29:07 +0800

    office 20220812

---
+++ SelfStudy/P03Indexer/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P03Indexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student1 student1 = new Student1();
+            student1.Name = "lina";
+            Console.WriteLine(student1.Name);
+
+            Student2 student2 = new Student2();
+            for (int i = 0; i < student2.listStudent.Count; i++)
+            {
+                student2[i].Name += "...";
+                Console.WriteLine(student2[i].Name);
+            }
+
+            Student3 student3 = new Student3();
+            Student3 stu = student3[1, "lina"];
+            Console.WriteLine("student number:" + stu.Sid + ",name:" + stu.Name + ",score:" + stu.Score);
+            student3[1, "lina"] = new Student3(111, "lina", 66);
+            stu = student3[111, "lina"];
+            Console.WriteLine("student number:" + stu.Sid + ",name:" + stu.Name + ",score:" + stu.Score);
+        }
+    }
+
+    public class Student1
+    {
+
+        private string name;
+        // property
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+
+    public class Student2
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public List<Student2> listStudent = new List<Student2>();
+
+        public Student2(string name)
+        {
+            this.name = name;
+        }
+        public Student2()
+        {
+            this.listStudent.Add(new Student2("lina1"));
+            this.listStudent.Add(new Student2("lina2"));
+            this.listStudent.Add(new Student2("lina3"));
+        }
+
+        public Student2 this[int index]
+        {
+            get { return listStudent[index]; }
+            set { listStudent[index] = value; }
+        }
+    }
+
+    // 构建多个参数的索引器
+    public class Student3
+    {
+
+        public List<Student3> listStudent3 = new List<Student3>();
+
+        private int sid;
+
+        public int Sid
+        {
+            get { return sid; }
+            set { sid = value; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int score;
+
+        public int Score
+        {
+            get { return score; }
+            set { score = value; }
+        }
+
+        public Student3(int sid, string name, int score)
+        {
+            this.sid = sid;
+            this.name = name;
+            this.score = score;
+        }
+
+        public Student3()
+        {
+            this.listStudent3.Add(new Student3(1, "lina", 88));
+            this.listStudent3.Add(new Student3(2, "lina", 90));
+            this.listStudent3.Add(new Student3(3, "lina", 98));
+        }
+
+        public Student3 this[int i, string name]
+        {
+            get 
+            {
+                foreach (var stu in listStudent3.ToArray())
+                {
+                    if(stu.sid == i && stu.name == name)
+                    {
+                        return stu;
+                    }
+                }
+                return null;
+            }
+
+            set { listStudent3[i] = value; }
+        }
+    }
+}
---
+++ SelfStudy/P04Delegate/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace P04Delegate
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            NumberChanger nc1 = new NumberChanger(Delegate1.AddNum);
+            NumberChanger nc2 = new NumberChanger(Delegate1.MultNum);
+            nc1(25);
+            Console.WriteLine("Value of Num: {0}", Delegate1.GetNum());
+            nc2(3);
+            Console.WriteLine("Value of Num: {0}", Delegate1.GetNum());
+
+            // 委托的多播
+            NumberChanger nc;
+            nc = nc1;
+            nc += nc2;
+            nc(3);
+            Console.WriteLine("Value of Num: {0}", Delegate1.GetNum());
+
+            //
+            MyDelegate deleg = Delegate2.MethodA;
+            Delegate2.InvokeDelagete(deleg);
+            deleg = Delegate2.MethodB;
+            Delegate2.InvokeDelagete(deleg);
+
+            add<int> sum = Delegate3.Sum;
+            Console.WriteLine(sum(10, 20));
+            add<string> con = Delegate3.Concat;
+            Console.WriteLine(con("Hello ", "World!!"));
+
+        }
+    }
+
+    // 委托的声明、实例化和使用
+    delegate int NumberChanger(int n);
+    public class Delegate1
+    {
+        static int num = 10;
+
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+        
+        public static int MultNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+
+        public static int GetNum()
+        {
+            return num;
+        }
+    }
+
+    public delegate void MyDelegate(string msg);
+    public class Delegate2
+    {
+        public static void MethodA(string message)
+        {
+            Console.WriteLine("Called ClassA.MethodA() with parameter: " + message);
+        }
+
+        public static void MethodB(string message)
+        {
+            Console.WriteLine("Called ClassA.MethodB() with parameter: " + message);
+        }
+
+        public static void InvokeDelagete(MyDelegate deleg){
+            deleg("hi,,,");
+        }
+    }
+
+    // 泛型委托
+    public delegate T add<T>(T param1, T param2);
+    public class Delegate3
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static string Concat(string a, string b)
+        {
+            return a + b;
+        }
+    }
+
+}

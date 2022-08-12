242fff10c5fcf41bf38b87b68e1b7123b0705331
Chunlong Xu <chunlongxu@microsoft.com>
2022-08-13 07:56:28 +0800

    liutiemeng P03

---
+++ Liutiemeng/P03E01ClassLibrary1/Calculater.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace P03E02ClassLibrary1
+{
+    public class Calculater
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Mul(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
--- Liutiemeng/P03E01ConsoleApp/Program.cs
+++ Liutiemeng/P03E01ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using P03E02ClassLibrary1;
+using System;
+
 
 namespace P03E01ConsoleApp
 {
@@ -6,7 +8,21 @@ namespace P03E01ConsoleApp
     {
         static void Main(string[] args)
         {
+            // 别人的 dll
             Console.WriteLine("Hello World!");
+            ClassLibraryTest.MyOperate op = new ClassLibraryTest.MyOperate();
+            Console.WriteLine("sum = {0}", op.GetSum(3, 5));
+            Console.WriteLine("sum = {0}", op.GetSubtract(3, 5));
+            // 公开的 dll : System.Windows.Froms
+
+            // nuget : Entity Framwork
+
+            // 白盒的话，先把其他solotion 下的 project 加入到 当前 solution 然后 add preference
+
+            // 建立自己的 libray, 编译后的结果就是 dll, 也需要 add proference
+            double res = Calculater.Mul(1.2, 5.2);
+            Console.WriteLine("res = {0}", res);
+
         }
     }
 }
